bd8f91ee83ff104361c08a2bf5cf5ea352cf8bfa
eigenbuzz <eigenbuzz@gmail.com>
2017-09-13 02:30:44 -0700

    tttttt

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -21,6 +21,10 @@ from email_preprocess import preprocess
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
+
+
+
+
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
